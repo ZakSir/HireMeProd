4ab8060957aa2da5cd5c1d077dd823f80df0ab91
Zak <hirezakfargo@gmail.com>
2018-04-05 22:36:01 -0700

    build changes

--- src/HireMe/HireMe.Functions/Build.cs
+++ src/HireMe/HireMe.Functions/Build.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
 namespace HireMe.Functions
 {
     public static class Build
     {
-        public Build()
+        [FunctionName("BuildTrigger")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "GET", "POST")] HttpRequestMessage req,
+            TraceWriter log)
         {
+            log.Info("first");
+
+            return req.CreateResponse(HttpStatusCode.OK, "First");
         }
     }
 }
